ba96d91de3a59da91bc105ae3584be71529b0dc1
itggot-timothy-nilsson <timothy.nilsson@itggot.se>
2018-04-27 13:58:09 +0200

    Fixed layout and index
    Gjorde klart layout samt la till layout bilder och började lite smått på index sidan

---
+++ Slutprojekt/site/js/menu.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    var element = document.querySelector(".close");
+    element.classList.toggle("show");
+
+    element = document.querySelector(".fade-layer");
+    element.classList.toggle("fade");
+}
